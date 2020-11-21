2db2aa2abee1ce2122eabdf2f3a2ad2d9b9e975d
Islam Mezied <imezied@gmail.com>
2020-11-21 12:10:28 +0200

    add plugin settings page

--- login-jobs.php
+++ login-jobs.php
@@ -28,7 +28,6 @@ if (!function_exists('loginjobs_setup_post_type')) {
         }
     }
 }
-
 add_action('init', 'loginjobs_setup_post_type');
 
 /**
@@ -54,3 +53,70 @@ function loginjobs_deactivate()
     flush_rewrite_rules();
 }
 register_deactivation_hook(__FILE__, 'loginjobs_deactivate');
+
+/**
+ * add plugin options page
+ */
+function loginjobs_add_settings_page()
+{
+    add_options_page(
+        'Jobs Settings Page',
+        'LOGIN Jobs',
+        'manage_options',
+        'login-jobs-settings-page',
+        'loginjobs_render_plugin_settings_page'
+    );
+}
+add_action('admin_menu', 'loginjobs_add_settings_page');
+
+/**
+ * Plugin setting page content
+ */
+function loginjobs_render_plugin_settings_page()
+{
+?>
+    <h2>Jobs Settings Page</h2>
+    <form action="options.php" method="post">
+        <?php
+        settings_fields('loginjobs_jobs_options_group');
+        do_settings_sections('login-jobs-settings-page'); ?>
+        <input name="submit" class="button button-primary" type="submit" value="<?php esc_attr_e('Save'); ?>" />
+    </form>
+<?php
+}
+
+
+function loginjobs_register_settings()
+{
+    add_option('job_close_duration');
+
+    register_setting('loginjobs_jobs_options_group', 'job_close_duration');
+
+    add_settings_section(
+        'loginjaobs_jobs_section_options',
+        'Jobs Options',
+        'loginjobs_plugin_section_text',
+        'login-jobs-settings-page'
+    );
+
+    add_settings_field(
+        'loginjobs_jobs_close_duration',
+        'Jobs Close Duration',
+        'loginjobs_plugin_setting_close_duration',
+        'login-jobs-settings-page',
+        'loginjaobs_jobs_section_options'
+    );
+}
+add_action('admin_init', 'loginjobs_register_settings');
+
+
+function loginjobs_plugin_setting_close_duration()
+{
+    $options = get_option('job_close_duration');
+    echo "<input id='loginjobs_plugin_setting_close_duration' name='job_close_duration' type='number' value='{$options}' /> Days";
+}
+
+function loginjobs_plugin_section_text()
+{
+    echo '<p>Here you can set all the options</p>';
+}
